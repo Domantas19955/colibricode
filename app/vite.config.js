bc3f849f25223ad7f132d1c8fe38a8b623e9cc8f
Domantas Stučinskas <domantas.stucinskas@robotikosakademija.lt>
2022-11-15 12:49:49 +0200

    vite config cleanup

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	esbuild: {
 		jsxFactory: 'h',
 		jsxFragment: 'Fragment'
 	  },
-  plugins: [react()],
-  base: './'
-//   resolve: {
-//     alias: {
-//       '/': path.resolve(__dirname, './src'),
-//     },
-//   },
+  plugins: [react()]
 })
